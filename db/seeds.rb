86fa6f676a2cff4ab4f4b13f35d04f0fd57d9411
Viktoriia Stoichuk <v.stoichuk@gmail.com>
2021-06-05 15:30:57 +0800

    completed design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
+data = JSON.parse(open(url).read)
+data["results"].each do |result| 
+  title = result["original_title"]
+  overview = result["overview"]
+  poster_path = result["poster_path"]
+  poster_url = "https://image.tmdb.org/t/p/w500#{poster_path}"
+  rating = result["vote_average"]
+  movie = Movie.new(title: "#{title}", overview: "#{overview}", poster_url: "#{poster_url}", rating: "#{rating}")
+  movie.save
+end
